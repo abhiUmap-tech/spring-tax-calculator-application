6cb373ddbc336b2eb08e6af2a32be74f1356a45a
abhishekumap <abhiumap31@gmail.com>
2026-06-24 10:46:33 +0530

    add Spring Tax Calculator App

--- docker/app1-hello/springboot/hello-spring/src/main/java/com/example/hello/HelloController.java
+++ docker/app1-hello/springboot/hello-spring/src/main/java/com/example/hello/HelloController.java
@@ -1,11 +1,18 @@
 package com.example.hello;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import java.net.InetAddress;
 import java.util.Map;
 @RestController
 public class HelloController {
+
+  @Value("${app.env.value}")
+  private String env;
+
+
   @GetMapping("/")
   public Map<String, String> hello() throws Exception {
+
     String env = System.getenv().getOrDefault("ENV_VALUE", "No env set");
     String hostname = InetAddress.getLocalHost().getHostName();
     
